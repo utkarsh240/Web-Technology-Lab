3564f64560fa56bcb81344c5fd61bcf14b03a155
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2023-03-03 12:31:48 +0530

    Create q5.java

---
+++ march3/q5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class q5 {
+    public static class base{
+        int a;
+        base(int a){
+            this.a = a;
+        }
+    }
+    public static class derived extends base{
+        int b;
+        derived(int a, int b){
+            super(a);
+            this.b = b;
+        }
+        void print(){
+            System.out.println("a : " + a + " " + " b : " + b);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the value of a : ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); // cin >> a;
+        System.out.println("Enter the value of b : ");
+        int b = sc.nextInt();
+
+        derived d = new derived(a, b);
+        d.print();
+    }
+}
