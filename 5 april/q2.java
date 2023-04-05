5750362f543946a856a1208e78d45ac28e078b55
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2023-04-05 12:33:28 +0530

    Create q2.java

---
+++ 5 april/q2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        System.out.println("Permutations of string: ");
+        permute(str, 0, str.length() - 1);
+        sc.close();
+    }
+
+    public static void permute(String str, int l, int r) {
+        if (l == r)
+            System.out.println(str);
+        else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permute(str, l + 1, r);
+                str = swap(str, l, i);
+            }
+        }
+    }
+
+    public static String swap(String str, int i, int j) {
+        char temp;
+        char[] charArray = str.toCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+}
